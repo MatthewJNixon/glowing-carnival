c93a62a225f57b02bbcfcf49fc73e08351518c6a
Matthew Nixon <matthew.nixon@arkahna.io>
2022-02-07 15:24:51 +0800

    removed pragma warning

--- FunctionApp1/Program.cs
+++ FunctionApp1/Program.cs
@@ -1,20 +1,15 @@
-﻿using FunctionApp1.Services;
+﻿using FunctionApp1;
+using FunctionApp1.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-#pragma warning disable CS0657
-[assembly: FunctionsStartup(typeof(FunctionApp1.Program))]
-#pragma warning restore CS0657
+[assembly: FunctionsStartup(typeof(Program))]
 namespace FunctionApp1;
 
 public class Program : FunctionsStartup
 {
-
     public override void Configure(IFunctionsHostBuilder builder)
     {
-
-        // builder.Services.AddTransient<IDataverseWebApiService>(_ => new DataverseWebApiService());
         builder.Services.AddTransient<IDataverseWebApiService, DataverseWebApiService>();
     }
 }
